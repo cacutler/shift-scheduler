3a028cb8ff2260ceb7f7b84336072cc1da225e3f
Alex Cutler <cacutler@gmail.com>
2026-04-24 17:04:24 -0600

    Updated code styling

--- app/Actions/Fortify/ResetUserPassword.php
+++ app/Actions/Fortify/ResetUserPassword.php
@@ -1,29 +1,21 @@
 <?php
-
 namespace App\Actions\Fortify;
-
 use App\Concerns\PasswordValidationRules;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\ResetsUserPasswords;
-
-class ResetUserPassword implements ResetsUserPasswords
-{
+class ResetUserPassword implements ResetsUserPasswords {
     use PasswordValidationRules;
-
     /**
      * Validate and reset the user's forgotten password.
-     *
      * @param  array<string, string>  $input
      */
-    public function reset(User $user, array $input): void
-    {
+    public function reset(User $user, array $input): void {
         Validator::make($input, [
-            'password' => $this->passwordRules(),
+            'password' => $this->passwordRules()
         ])->validate();
-
         $user->forceFill([
-            'password' => $input['password'],
+            'password' => $input['password']
         ])->save();
     }
 }
--- app/Http/Requests/ShiftStoreRequest.php
+++ app/Http/Requests/ShiftStoreRequest.php
@@ -1,27 +1,19 @@
 <?php
-
 namespace App\Http\Requests;
-
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
-
-class ShiftStoreRequest extends FormRequest
-{
+class ShiftStoreRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
+    public function authorize(): bool {
         return false;
     }
-
     /**
      * Get the validation rules that apply to the request.
-     *
      * @return array<string, ValidationRule|array<mixed>|string>
      */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
             //
         ];
